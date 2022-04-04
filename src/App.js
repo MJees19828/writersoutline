c48854a93f61f1860f98d68341050d677d31c2e0
alhassanabdul-majeed <maj.baanga60@gmail.com>
2022-04-04 13:32:24 +0100

    added writersoutline

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import outlineCard from './components/outlineCard';
 
-function App() {
+import React, {useState} from 'react'
+
+ function App() {
+const [writers,setWriters] = useState({
+  loading: false,
+  list:[]
+});
+
+const handleClick =()=>{
+  setWriters((previousState)=>({
+    ...previousState,
+    loading:true
+  }))
+setTimeout(async()=>{
+  let resp =await fetch("/writers.json");
+  let result =await resp.json();
+
+  setWriters((previousState)=>({
+    ...previousState,
+    loading:false,
+    list:result
+  }));
+}, 2500)
+
+};
+
+
+  if  (writers.loading){
+    return(
+      <div>
+      <h1>Writers Profile</h1>
+<div className='container'>
+<div className='card action'>
+<p className='infoText'>Loading...</p>
+
+</div>
+</div>
+</div>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div>
+      <h1>Writers outline</h1>
+      <div className='container'>
+      
+    {writers.list.length === 0 ? (
+ <div className='card action'>
+ <p className='infoText'>Oops... no writer profile found</p>
+<button className='actionBtn' onClick={handleClick}>Get Writers</button>
+      </div>
+    ):(
+writers.list.map((writer) =>(
+  <ProfileCard key={writer.id} writer={writer}/>
+))
+    )}
+      </div>
+      
+      
+        </div>
+    );
+  }
 
 export default App;
